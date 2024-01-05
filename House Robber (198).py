b7767c36cfc4c91a7ec568be2bfeea769243b3be
Jared Cazel <jjcazel@gmail.com>
2024-01-05 09:50:28 -0500

    Update House Robber (198).py

--- House Robber (198).py
+++ House Robber (198).py
@@ -4,6 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # O(2^n) time | O(2^n) space
         # if len(nums) == 1: #
         #     return nums[0]
         # if len(nums) == 2:
@@ -11,6 +12,7 @@ class Solution(object):
         
         # return max((nums[0] + self.rob(nums[2:])), self.rob(nums[1:]))
 
+        #O(n) time | O(n) space
         rob_house = 0
         skip_house = 0
 
