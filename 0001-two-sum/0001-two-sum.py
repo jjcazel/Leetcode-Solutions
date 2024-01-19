2b3632a976f096a8cf7e6889ecb7d3eb1b941748
Jared Cazel <jjcazel@gmail.com>
2024-01-19 12:22:58 -0500

    Time: 48 ms (98.82%), Space: 17.9 MB (53.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -6,7 +6,6 @@ class Solution:
         for idx in range(len(nums)):
             curr_num = nums[idx] # 3
             difference = target - curr_num # 3
-            if difference not in differences_dict:
-                differences_dict[difference] = idx
-            if curr_num in differences_dict and idx != differences_dict[curr_num]:
+            if curr_num in differences_dict:
                 return [differences_dict[curr_num], idx]
+            differences_dict[difference] = idx
