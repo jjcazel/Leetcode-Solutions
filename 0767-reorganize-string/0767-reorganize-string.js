75a7703e83e7b87fb6f43b9de8c58172eea138f4
Jared Cazel <jjcazel@gmail.com>
2024-03-20 19:03:48 -0400

    Time: 56 ms (84.57%), Space: 51.8 MB (64.61%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.js
+++ 0767-reorganize-string/0767-reorganize-string.js
@@ -5,29 +5,26 @@
 
 // O(n) time and O(n) space where n is the length of the input string
 function reorganizeString(s) {
-  const seenCharCounts = {}; // {a: 3, b: 1}
+  const seenCharCounts = {};
 
-  for (let i = 0; i < s.length; i++) {
-    const currChar = s[i];
-    if (!seenCharCounts.hasOwnProperty(currChar)) {
-      seenCharCounts[currChar] = 0;
+  for (const char of s) {
+    if (!seenCharCounts.hasOwnProperty(char)) {
+      seenCharCounts[char] = 0;
     }
-    seenCharCounts[currChar]++;
+    seenCharCounts[char]++;
   }
 
   const charArray = Object.entries(seenCharCounts);
-  console.log(Object.entries(seenCharCounts));
   charArray.sort((a, b) => b[1] - a[1]); // Sort by frequency, descending
-
   if (charArray[0][1] > Math.floor((s.length + 1) / 2)) {
-    return ''; // Early return if the condition is not met
+    return '';
   }
 
   let evenIndex = 0, oddIndex = 1;
   const result = new Array(s.length);
 
-  for (const [char, initialCount] of charArray) {
-    let count = initialCount;
+  for (const [char, frequency] of charArray) {
+    let count = frequency;
     while (count > 0) {
       if (evenIndex < s.length) {
         result[evenIndex] = char;
