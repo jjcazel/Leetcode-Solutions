ec6c321e57ab373050b31d5bb9870ee5cdbd283b
Jared Cazel <jjcazel@gmail.com>
2024-03-21 22:39:48 -0400

    Time: 60 ms (57.87%), Space: 52.4 MB (35.93%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -3,33 +3,21 @@
  * @return {number}
  */
 
-// O(n) time and O(n) space where n is the length of height array (input)
+// O(n) time and O(1) space where n is the length of height array (input)
 function trap(height) {
   let rainWater = 0; // 6
+  let [left, right] = [0, height.length - 1];
+  let [maxLeft, maxRight] = [height[0], height[height.length - 1]];
 
-  let rightMaxes = new Array(height.length);
-  let leftMaxes = new Array(height.length);
-  let minAmounts = new Array(height.length);
-
-  let currMax = height[0];
-  for (let i = 0; i < height.length; i++) {
-    leftMaxes[i] = currMax;
-    if (height[i] > currMax) {
-      currMax = height[i];
-    }
-  }
-
-  currMax = height[height.length - 1];
-  for (let j = height.length - 1; j >= 0; j--) {
-    rightMaxes[j] = currMax;
-    if (height[j] > currMax) {
-      currMax = height[j];
-    }
-  }
-
-  for (let k = 0; k < rightMaxes.length; k++) {
-    if (Math.min(rightMaxes[k], leftMaxes[k]) - height[k] > 0) {
-      rainWater += Math.min(rightMaxes[k], leftMaxes[k]) - height[k];
+  while (left < right) {
+    if (maxLeft < maxRight) {
+      left++;
+      maxLeft = Math.max(maxLeft, height[left]);
+      rainWater += maxLeft - height[left]
+    } else {
+      right--;
+      maxRight = Math.max(maxRight, height[right]);
+      rainWater += maxRight - height[right];
     }
   }
 
