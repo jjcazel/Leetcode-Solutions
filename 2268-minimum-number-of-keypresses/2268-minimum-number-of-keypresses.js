d0b97eb2cf3806098362fd6b086c03b3d1377cd5
Jared Cazel <jjcazel@gmail.com>
2024-03-22 14:46:31 -0400

    Time: 84 ms (38.54%), Space: 57 MB (27.08%) - LeetHub

--- 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.js
+++ 2268-minimum-number-of-keypresses/2268-minimum-number-of-keypresses.js
@@ -5,25 +5,26 @@
 
 // O(1) time and O(1) space becasue the problem only deals with 26 possible letters
 function minimumKeypresses(s) {
-  const freqTable = {};
+  const frequencyMap = {};
 
-  for (const char of s) {
-    if (!freqTable.hasOwnProperty(char)) {
-      freqTable[char] = 0;
+  for (let char of s) {
+    if(!frequencyMap.hasOwnProperty(char)) {
+        frequencyMap[char] = 0;
     }
-    freqTable[char]++;
+    frequencyMap[char]++;
   }
 
-  const frequencyArr = Object.values(freqTable).sort((a, b) => b - a);
-  let numMinKeypresses = 0;
+  const freqArr = Object.values(frequencyMap).sort((a, b) => b - a);
+  let minKeyPresses = 0;
 
-  numMinKeypresses += frequencyArr.slice(0, 9).reduce((sum, num) => sum += num);
-  if (frequencyArr.length > 9) {
-    numMinKeypresses += frequencyArr.slice(9, 18).reduce((sum, num) => sum += num) * 2;
+  minKeyPresses += freqArr.slice(0, 9).reduce((sum, num) => sum += num);
+
+  if (freqArr.length > 9) {
+    minKeyPresses += freqArr.slice(9, 18).reduce((sum, num) => sum += num) * 2;
   }
-  if (frequencyArr.length > 18) {
-    numMinKeypresses += frequencyArr.slice(18).reduce((sum, num) => sum += num) * 3;
+  if (freqArr.length > 18) {
+    minKeyPresses += freqArr.slice(18).reduce((sum, num) => sum += num) * 3;
   }
 
-  return numMinKeypresses;
+  return minKeyPresses;
 };
