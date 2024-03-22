701b68e73a52bef0f4901d9ace90288f9845e289
Jared Cazel <jjcazel@gmail.com>
2024-03-22 16:31:48 -0400

    Time: 54 ms (56.73%), Space: 49.2 MB (34.56%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -3,24 +3,9 @@
  * @return {number}
  */
 
-// O(n^2) time and O(n) space
+// O(n) time and O(n) space
 function minimumOperations(nums) {
-  let numOperations = 0;
-  let currSum = nums.reduce((sum, num) => sum + num, 0);
-
-  while (currSum > 0) {
-    let nonZeroNums = nums.filter((num) => num !== 0);
-    let currMin = Math.min(...nonZeroNums);
-    currSum = 0; // Reset currSum to recalculate it after the subtraction
-
-    for (let i = 0; i < nums.length; i++) {
-      if (nums[i] !== 0) {
-        nums[i] -= currMin;
-      }
-      currSum += nums[i]; // Recalculate currSum with updated values
-    }
-    numOperations++;
-  }
-
-  return numOperations;
+  const nonZeros = nums.filter(num => num !== 0);
+  const uniqueSet = new Set(nonZeros);
+  return uniqueSet.size;
 };
