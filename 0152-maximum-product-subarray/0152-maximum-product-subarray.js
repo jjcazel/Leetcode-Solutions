45bc981d3b2a15c54909a391f370fe228143722c
Jared Cazel <jjcazel@gmail.com>
2024-04-13 16:04:40 -0400

    Time: 66 ms (24.57%), Space: 51.3 MB (24.98%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -5,13 +5,13 @@
 
  // O(n) time and O(1) space where n is the length of nums
 const maxProduct = (nums) => {
-    let maxProduct = Math.max(...nums); // 0
-    let currMax = 1;
-    let currMin = 1; // 0
+    let maxProduct = Math.max(...nums); // 4
+    let currMax = 1; // 2
+    let currMin = 1; // 2
     
     for (const num of nums) {
-        tempMax = num * currMax; // 0
-        currMax = Math.max(num, num * currMax, num * currMin);
+        tempMax = num * currMax; // 6
+        currMax = Math.max(num, tempMax, num * currMin);
         currMin = Math.min(num, tempMax, num * currMin); 
         
         maxProduct = Math.max(currMax, maxProduct);
