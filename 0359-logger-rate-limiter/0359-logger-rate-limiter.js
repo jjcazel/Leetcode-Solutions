283e008cd162825acdc6b4a5c89e7f30f332fc7a
Jared Cazel <jjcazel@gmail.com>
2024-04-19 15:18:51 -0400

    Time: 147 ms (10.76%), Space: 62.2 MB (81.4%) - LeetHub

---
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.js
@@ -0,0 +1,26 @@
+
+var Logger = function () {
+    this.logTimeStamps = {};
+};
+
+/** 
+ * @param {number} timestamp 
+ * @param {string} message
+ * @return {boolean}
+ */
+Logger.prototype.shouldPrintMessage = function (timestamp, message) {
+    if (
+        this.logTimeStamps.hasOwnProperty(message) && timestamp < this.logTimeStamps[message] + 10
+    ) {
+        return false;
+    }
+    
+    this.logTimeStamps[message] = timestamp;
+    return true;
+};
+
+/** 
+ * Your Logger object will be instantiated and called as such:
+ * var obj = new Logger()
+ * var param_1 = obj.shouldPrintMessage(timestamp,message)
+ */
