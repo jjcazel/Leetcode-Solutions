f82d2476bfd2e31b15e0c9d3aa71595242932cd0
Jared Cazel <jjcazel@gmail.com>
2024-04-21 16:26:29 -0400

    Time: 55 ms (74.11%), Space: 51.2 MB (39.34%) - LeetHub

---
+++ 2700-differences-between-two-objects/2700-differences-between-two-objects.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Object|Array} obj1
+ * @param {Object|Array} obj2
+ * @return {Object|Array}
+ */
+
+function objDiff(obj1, obj2) {
+    if (obj1 === obj2) return {};
+    if (obj1 == null || obj2 == null) return [obj1, obj2];
+    if (typeof obj1 !== "object" || typeof obj2 !== "object") return [obj1, obj2];
+    if (Array.isArray(obj1) !== Array.isArray(obj2)) return [obj1, obj2];
+
+    const differences = {};
+    for (const key in obj1) {
+        if (obj2.hasOwnProperty(key)) {
+            const subDiff = objDiff(obj1[key], obj2[key]);
+            if (Object.keys(subDiff).length > 0) {
+                differences[key] = subDiff;
+            }
+        }
+    }
+    return differences;
+};
